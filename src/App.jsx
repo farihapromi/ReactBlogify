81d60b735fea7627d746c9c6a0b0225b83bad3e8
Fariha Promi <farihapromi2611@gmail.com>
2025-05-13 20:53:52 +0600

    Add json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
  
 
   return (
     <>
-    <h1 className='text-2xl bg-red-400'>Knowledge Cafe</h1>
+    <Header/>
+   
      
     </>
   )
